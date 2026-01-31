70257dcd2dffac811e4e5524585c42ca3ec8fd28
yamtanjour <yamtanjour@gmail.com>
2026-01-31 22:55:52 +0100

    added bluetooth and started App

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BreathWatch',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('BreathWatch')),
+      body: const Center(
+        child: Text('Sensor data will appear here\n(TODO: Add graphs, pictures, AI)', textAlign: TextAlign.center),
+      ),
+    );
+  }
+}
